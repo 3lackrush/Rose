afafa0ea878131b0673cf8b533ddcf8906816f04
3lackrush <505478266@qq.com>
2018-01-02 13:31:06 +0800

    add argparse in rose.py

--- rose.py
+++ rose.py
@@ -10,11 +10,16 @@ Copyright (c) 2017
 import sys
 from lib.core.Spider import SpiderMain
 from lib.welcome.welcome import welcome
+import argparse
 
 
 def main():
-    root = "https://www.mkernel.com"
-    threadNum = 10
+    parser = argparse.ArgumentParser(description='Rose Scanner Help.')
+    parser.add_argument("-t", "--target", type=str, help="Add root url of the target site!")
+    parser.add_argument("-n", '--number', type=int, help="Add thread number!")
+    args = parser.parse_args()
+    root = args.target
+    threadNum = args.number
 
     crawler1 = SpiderMain(root, threadNum)
     crawler1.craw()
