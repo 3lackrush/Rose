8f0aa1bb6a822a47ec8714fcc85e49adf5d111f4
3lackrush <505478266@qq.com>
2018-01-02 13:55:11 +0800

    add User-agent settings

--- lib/core/Download.py
+++ lib/core/Download.py
@@ -6,14 +6,16 @@ import requests
 class Downloader(object):
 
 	def get(self, url):
-		r = requests.get(url,timeout=10)
+		user_agent = {'User-agent': 'Mozilla/5.0'}
+		r = requests.get(url,headers = user_agent, timeout=10)
 		if r.status_code != 200:
 			return None
 		_str = r.text
 		return _str
 
 	def post(self, url, data):
-		r = requests.post(url,data)
+		user_agent = {'User-agent': 'Mozilla/5.0'}
+		r = requests.post(url, headers=user_agent, data)
 		_str = r.text
 		return _str
 
@@ -24,7 +26,8 @@ class Downloader(object):
 		_str = {}
 		_str["url"] = url
 		try:
-			r = requests.get(url, timeout=10)
+			user_agent = {'User-agent': 'Mozilla/5.0'}
+			r = requests.get(url, headers=user_agent, timeout=10)
 			if r.status_code != 200:
 				return None
 			_str["html"] = r.text
