12d1b97b0018471b10ea596627bed3923c2ce603
3lackrush <505478266@qq.com>
2018-01-02 11:24:12 +0800

    Add exception in email_check

--- script/email_check.py
+++ script/email_check.py
@@ -8,15 +8,18 @@ from termcolor import colored
 class spider():
 
 	def run(self, url, html):
-		real_email_list = []
-		pattern = re.compile(r'([\w-]+@[\w-]+\.[\w-]+)+')
-		email_list = re.findall(pattern, html)
-		avoid_pic = ['png','jpeg','ico','bmp','PNG','JPEG','ICO','BMP']
-		for _email in email_list:
-			if _email.split(".")[-1] in avoid_pic:
-				pass
-			else:
-				real_email_list.append(_email)
+		try:
+			real_email_list = []
+			pattern = re.compile(r'([\w-]+@[\w-]+\.[\w-]+)+')
+			email_list = re.findall(pattern, html)
+			avoid_pic = ['png','jpeg','ico','bmp','PNG','JPEG','ICO','BMP']
+			for _email in email_list:
+				if _email.split(".")[-1] in avoid_pic:
+					pass
+				else:
+					real_email_list.append(_email)
+		except Exception as e:
+			pass
 
 		if(real_email_list):
 			print(colored("  [+] Email Address Found! ","red"), real_email_list)
